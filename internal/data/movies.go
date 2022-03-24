66cc0fbe0d9db1a7da5829cafc3f59b2602601d9
Sekib Omazic <sekib.omazic@gmail.com>
2022-03-24 07:43:13 +0100

    implement list filtering

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -95,6 +95,8 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
     SELECT id, created_at, title, year, runtime, genres, version
     FROM movies
+    WHERE (LOWER(title)=LOWER($1) OR $1 = '')
+    AND (genres @> $2 OR genres = '{}')
     ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
@@ -102,7 +104,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 
 	// Use QueryContext() to execute the query. This returns a sql.Rows resultset
 	// containing the result
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
