ac17f62ddcf3fc198ec752d39147f8a6d6472630
Sekib Omazic <sekib.omazic@gmail.com>
2022-03-27 22:15:57 +0200

    implement global rate limiting

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -32,5 +32,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
 	// Return router instance
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.4
 )
+
+require golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
